4b407a945a4fafb94b79b9af9c8317b90b3c2f42
Elizabeth Coble <elizabethjiamei@gmail.com>
2021-03-16 03:56:46 -0500

    IR transmission working with LIRC

---
+++ send.py
@@ -0,0 +1,135 @@
+import os
+
+def power():
+    os.system("irsend SEND_ONCE ledall KEY_1")
+
+def pause():
+    os.system("irsend SEND_ONCE ledall KEY_2")
+
+def bright_down():
+    os.system("irsend SEND_ONCE ledall KEY_3")
+
+def bright_up():
+    os.system("irsend SEND_ONCE ledall KEY_4")
+
+def red():
+    os.system("irsend SEND_ONCE ledall KEY_A")
+
+def orange():
+    os.system("irsend SEND_ONCE ledall KEY_B")
+
+def dark_yellow():
+    os.system("irsend SEND_ONCE ledall KEY_C")
+
+def yellow():
+    os.system("irsend SEND_ONCE ledall KEY_D")
+
+def light_yellow():
+    os.system("irsend SEND_ONCE ledall KEY_E")
+
+def green():
+    os.system("irsend SEND_ONCE ledall KEY_F")
+
+def light_green():
+    os.system("irsend SEND_ONCE ledall KEY_G")
+
+def cyan():
+    os.system("irsend SEND_ONCE ledall KEY_H")
+
+def light_blue():
+    os.system("irsend SEND_ONCE ledall KEY_I")
+
+def sky_blue():
+    os.system("irsend SEND_ONCE ledall KEY_J")
+
+def blue():
+    os.system("irsend SEND_ONCE ledall KEY_K")
+
+def dark_blue():
+    os.system("irsend SEND_ONCE ledall KEY_L")
+
+def lyons_blue():
+    os.system("irsend SEND_ONCE ledall KEY_M")
+
+def purple():
+    os.system("irsend SEND_ONCE ledall KEY_N")
+
+def light_purple():
+    os.system("irsend SEND_ONCE ledall KEY_O")
+
+def white():
+    os.system("irsend SEND_ONCE ledall KEY_P")
+
+def milk_white():
+    os.system("irsend SEND_ONCE ledall KEY_Q")
+
+def white_pink():
+    os.system("irsend SEND_ONCE ledall KEY_R")
+
+def green_white():
+    os.system("irsend SEND_ONCE ledall KEY_S")
+
+def blue_white():
+    os.system("irsend SEND_ONCE ledall KEY_T")
+
+def inc_red():
+    os.system("irsend SEND_ONCE ledall KEY_F1")
+
+def dec_red():
+    os.system("irsend SEND_ONCE ledall KEY_F2")
+
+def inc_green():
+    os.system("irsend SEND_ONCE ledall KEY_F3")
+
+def dec_green():
+    os.system("irsend SEND_ONCE ledall KEY_F4")
+
+def inc_blue():
+    os.system("irsend SEND_ONCE ledall KEY_F5")
+
+def dec_blue():
+    os.system("irsend SEND_ONCE ledall KEY_F6")
+
+def diy1():
+    os.system("irsend SEND_ONCE ledall KEY_F7")
+
+def diy2():
+    os.system("irsend SEND_ONCE ledall KEY_F8")
+
+def diy3():
+    os.system("irsend SEND_ONCE ledall KEY_F9")
+
+def diy4():
+    os.system("irsend SEND_ONCE ledall KEY_F10")
+
+def diy5():
+    os.system("irsend SEND_ONCE ledall KEY_F11")
+
+def diy6():
+    os.system("irsend SEND_ONCE ledall KEY_F12")
+
+def speed_up():
+    os.system("irsend SEND_ONCE ledall KEY_F13")
+
+def slow_down():
+    os.system("irsend SEND_ONCE ledall KEY_F14")
+
+def auto():
+    os.system("irsend SEND_ONCE ledall KEY_F15")
+
+def flash_toggle():
+    os.system("irsend SEND_ONCE ledall KEY_F16")
+
+def jump_3():
+    os.system("irsend SEND_ONCE ledall KEY_F17")
+
+def jump_7():
+    os.system("irsend SEND_ONCE ledall KEY_F18")
+
+def fade_3():
+    os.system("irsend SEND_ONCE ledall KEY_F19")
+
+def fade_7():
+    os.system("irsend SEND_ONCE ledall KEY_F20")
+
+orange()
